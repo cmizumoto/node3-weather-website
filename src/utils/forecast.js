8d97c686b75d369093b0c7dbcc140ac187112231
Caio Mizumoto <665112@liq.com.br>
2019-11-06 10:25:20 -0300

    add more features to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. Also with max temperature of: ' + body.daily.data[0].temperatureHigh + ', and minimum of: ' + body.daily.data[0].temperatureLow + '.')
         }
     })
 }
 
-module.exports = forecast
+module.exports = forecast;
